205204101ddc23f9f69ddde398564662445f24af
tudoblu <paulo.santarem@yahoo.com.br>
2026-04-29 15:26:47 +0000

    Correcao plotar estereogramas e rose 03

--- graficos.py
+++ graficos.py
@@ -1006,6 +1006,8 @@ def plotar_estereograma_e_rose(df_juntas, df_veios, afloramento_selecionado, cam
         return fig
 
     # Contorno de densidade (combinando juntas e veios)
+    min_density_level = 2 # Nível mínimo de densidade em % para começar a colorir
+    levels = np.arange(min_density_level, 100, 1) # Começa de 10% e vai de 2 em 2 até 100%
     st.write("DEBUG: Tipo de all_strikes_for_density:", type(all_strikes_for_density))
     st.write("DEBUG: Dtype de all_strikes_for_density:", all_strikes_for_density.dtype)
     st.write("DEBUG: Tem NaN em all_strikes_for_density?", all_strikes_for_density.isnull().any())
@@ -1013,8 +1015,8 @@ def plotar_estereograma_e_rose(df_juntas, df_veios, afloramento_selecionado, cam
     st.write("DEBUG: Dtype de all_dips_for_density:", all_dips_for_density.dtype)
     st.write("DEBUG: Tem NaN em all_dips_for_density?", all_dips_for_density.isnull().any())
 
-    min_density_level = 2 # Nível mínimo de densidade em % para começar a colorir
-    levels = np.arange(min_density_level, 100, 1) # Começa de 10% e vai de 2 em 2 até 100%
+    
+    
     all_strikes_for_density = pd.concat([df_final_juntas['Strike_RHR'], df_final_veios['Strike_RHR']])
     all_dips_for_density    = pd.concat([df_final_juntas['Dip'],        df_final_veios['Dip']])
 
